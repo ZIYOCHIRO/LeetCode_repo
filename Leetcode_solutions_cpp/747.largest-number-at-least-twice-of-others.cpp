dcb69a8f0bced840535ec459ddca96a554c2cea2
ziyo <ziyochiro@gmail.com>
2021-06-15 17:47:12 +0800

    10题

---
+++ Leetcode_solutions_cpp/747.largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=747 lang=cpp
+ *
+ * [747] Largest Number At Least Twice of Others
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
