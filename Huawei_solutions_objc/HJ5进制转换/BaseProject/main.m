639ea99c7426f05ca7becb022bd409debf5861ee
ZIYOCHIRO <ziyochiro@gmail.com>
2023-04-22 14:36:53 +0800

    update huawei

---
+++ Huawei_solutions_objc/HJ5进制转换/BaseProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseProject
+//
+//  Created by RuiSalli on 22/4/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
